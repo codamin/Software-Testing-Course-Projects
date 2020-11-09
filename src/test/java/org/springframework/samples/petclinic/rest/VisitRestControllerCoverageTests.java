a348b5543b73b6192c8478ca878c18666ba525ce
HoEnZa <h.entezari78@gmail.com>
2020-11-08 16:22:47 -0800

    Done

--- src/test/java/org/springframework/samples/petclinic/rest/VisitRestControllerCoverageTests.java
+++ src/test/java/org/springframework/samples/petclinic/rest/VisitRestControllerCoverageTests.java
@@ -10,12 +10,14 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.MockitoRule;
 import org.postgresql.core.VisibleBufferedInputStream;
 import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.model.Visit;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class VisitRestControllerCoverageTests {
@@ -29,33 +31,60 @@ public class VisitRestControllerCoverageTests {
 
 	@Test
 	public void UpdateVisit_NullVisit() {
+		when(bindingResultMock.hasErrors()).thenReturn(false);
+		Pet pet = new Pet();
 		visitRestControllerMock.updateVisit(1, null, bindingResultMock);
 	}
 
 	@Test
 	public void UpdateVisit_NullVisitGetPet() {
-		when(visitMock.getPet()).thenReturn(null);
-		visitRestControllerMock.updateVisit(1, visitMock, bindingResultMock);
+		when(bindingResultMock.hasErrors()).thenReturn(false);
+		lenient().when(visitMock.getPet()).thenReturn(null);
+		Visit visit = new Visit();
+		visitRestControllerMock.updateVisit(1, visit, bindingResultMock);
 	}
 
 	@Test
 	public void UpdateVisit_BindingResultHasError() {
 		Visit visit = new Visit();
 		when(bindingResultMock.hasErrors()).thenReturn(true);
+		Pet pet = new Pet();
+		lenient().when(visitMock.getPet()).thenReturn(pet);
 		visitRestControllerMock.updateVisit(1, visit, bindingResultMock);
 	}
 
 	@Test
-	public void UpdateOwner_NotFoundOwner() {
-		Visit visit = new Visit();
-		when(clinicServiceMock.findOwnerById(anyInt())).thenReturn(null);
-		visitRestControllerMock.updateVisit(1, visit, bindingResultMock);
+	public void UpdateVisit_BindingResultHasErrorNullVisit() {
+		when(bindingResultMock.hasErrors()).thenReturn(true);
+		Pet pet = new Pet();
+		visitRestControllerMock.updateVisit(1, null, bindingResultMock);
 	}
 
 	@Test
-	public void UpdateOwner_NoProblem() {
+	public void UpdateVisit_BindingResultsErrorNullVisitGetPet() {
+		lenient().when(bindingResultMock.hasErrors()).thenReturn(true);
+		lenient().when(visitMock.getPet()).thenReturn(null);
 		Visit visit = new Visit();
-		when(clinicServiceMock.findVisitById(anyInt())).thenReturn(visit);
-		visitRestControllerMock.updateVisit(1, visit, bindingResultMock);
+		visitRestControllerMock.updateVisit(1, visitMock, bindingResultMock);
+	}
+
+	@Test
+	public void UpdateVisit_NotFoundVisit() {
+		when(bindingResultMock.hasErrors()).thenReturn(false);
+		Pet pet = new Pet();
+		lenient().when(visitMock.getPet()).thenReturn(pet);
+		Visit visit = new Visit();
+		lenient().when(clinicServiceMock.findOwnerById(anyInt())).thenReturn(null);
+		visitRestControllerMock.updateVisit(1, visitMock, bindingResultMock);
+	}
+
+	@Test
+	public void UpdateVisit_NoProblem() {
+		when(bindingResultMock.hasErrors()).thenReturn(false);
+		Pet pet = new Pet();
+		lenient().when(visitMock.getPet()).thenReturn(pet);
+		Visit visit = new Visit();
+		lenient().when(clinicServiceMock.findVisitById(anyInt())).thenReturn(visit);
+		visitRestControllerMock.updateVisit(1, visitMock, bindingResultMock);
 	}
 }
